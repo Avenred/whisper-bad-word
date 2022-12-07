a51dc86ef95845616758185991e6baf8249e79c6
Avenred <13921970+Avenred@users.noreply.github.com>
2022-12-06 20:13:27 -0700

    Extra formatting
    YAY YAY YAY MERGE CONFLICTS GOOOO

--- custom.py
+++ custom.py
@@ -1,3 +1,5 @@
-def custom(ok): # define the custom function. It takes one argument. If this function is not present then an error will occur
-    print("I hate everything. Existence is pain.") # The custom code in question
-# Custom code
+def custom(ok):  # define the custom function. It takes one argument.
+    # If this function isn't present, the script will crash.
+    print("I hate everything. Existence is pain.")  # The custom code in question
+
+    # Custom code lives here
--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from onbad import BadFinder
-from colorama import Cursor
+
+# from colorama import Cursor  # unused?
 
 # Function placed in onbad.py
 
-handle = BadFinder(custom_script_file="custom.py") # initialize an instance of the class
-handle.begin() # start the threads
+handle = BadFinder(
+    custom_script_file="custom.py"
+)  # initialize an instance of the class
+handle.begin()  # start the threads
 while handle.Running:
     try:
-        print(end="") # just to catch errors
+        print(end="")  # just to catch errors
     except KeyboardInterrupt:
         handle.Running = False
         exit(0)
