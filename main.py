a18166a350e35d78efa9681e458815547a01c6ad
Avenred <13921970+Avenred@users.noreply.github.com>
2023-12-04 16:40:58 -0700

    Add checking for badwords, increase recording time to 15s
    Increased the recording time for the .wav files to 15s. Whisper can support up to 30s, but I feel this is a good compromise of both being responsive and accurate.
    The issue with checking too frequently is that words can sometimes be cut-off. This can still happen, but there are less cut-off words the longer the recording.
    Next up would be making a new file and a function within that file that executes when a bad word is detected. Almost there!!!

--- main.py
+++ main.py
@@ -7,54 +7,76 @@ import queue
 import os
 import whisper
 
+# TODO: Set up logging instead of print statements
 filename_queue = queue.Queue()  # Create a queue to store the filenames
+transcription_queue = queue.Queue()
 file_written_event = threading.Event()
-
+print("Loading model. This could take a while...")
+whisper_model = whisper.load_model("tiny.en")
+print("Loaded!")
 
 def record_audio(seconds):
     sample_rate = 44100
     while True:
         directory = "temp_recordings"
-        filename = f"{random.randint(0, 999999999)}.wav"  # Generate a random filename
+        filename = f"{random.randint(0, 999999999)}.wav"  # There's a 1/999999999 chance of a collision. Whoops.
         filepath = os.path.join(directory, str(filename))
-        # Start recording
         print("Recording...")
         recording = sd.rec(
             int(seconds * sample_rate), samplerate=sample_rate, channels=2
         )
-        time.sleep(seconds)  # Wait for the recording to finish
-        # TODO: See if we can do this without using os.fsync()
-        f = open(filepath, "wb")
-        # Save the recording to a file
-        wavwrite(f, sample_rate, recording)
-        os.fsync(f.fileno())
-        f.close()
+        time.sleep(seconds)  # To avoid writing to the file while it is being recorded
+        wavwrite(filepath, sample_rate, recording)
 
-        # Signal that the file has been written
+        # Helps to avoid a race condition where the file is read/attempted to be transcribed before it is written
         file_written_event.set()
+        print("Done recording!")
 
         # Put the filename into the queue
         filename_queue.put(filename)
 
 
 def process_audio():
-    print("Loading model. This could take a while...")
-    whisper_model = whisper.load_model("tiny.en")
-    print("Loaded!")
     while True:
         filename = filename_queue.get()
         file_written_event.wait()
         # print(filename)
         transcription = whisper_model.transcribe(f"temp_recordings/{filename}")
-        # TODO: Actually print the transcription's text
-        print(transcription)
+        print(transcription["text"])
+        transcription_queue.put(transcription["text"])
         file_written_event.clear()
 
 
+def return_badwords(transcription):
+    with open('badwords.txt') as f:
+        badwords = [line.strip().lower() for line in f.readlines()]
+    transcription = transcription.lower()
+    detected_badwords = [badword for badword in badwords if badword in transcription]
+    # We want to return both the badwords it detected and a boolean of whether badwords exist;
+    # my idea is to run a function in another file that will only run if badwords exist.
+    # I believe that function should have as much access to this file as possible to make development easier.
+    if len(detected_badwords) > 0:
+        badwords_exist = True
+        return badwords_exist, detected_badwords
+    else:
+        badwords_exist = False
+        detected_badwords = None
+        return badwords_exist, detected_badwords
+
+
+def check_badwords_string():
+    while True:
+        transcribe = transcription_queue.get()
+        badwords_exist, detected_badwords = return_badwords(transcribe)
+        if badwords_exist:
+            print(f"Bad word(s) detected! {detected_badwords}")
+
 # Create two threads, one for each task
-record_thread = threading.Thread(target=record_audio, args=(5,))
+record_thread = threading.Thread(target=record_audio, args=(15,))
 process_thread = threading.Thread(target=process_audio)
+check_thread = threading.Thread(target=check_badwords_string)
 
 # Start the threads!!!
 record_thread.start()
 process_thread.start()
+check_thread.start()
