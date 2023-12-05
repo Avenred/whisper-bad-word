ec03dc4b4b16dc80ae132ce4cbb7f2d2f862e2bd
Avenred <13921970+Avenred@users.noreply.github.com>
2023-12-05 11:59:22 -0700

    Added debug logging and additional config
    - You can specify the recording time and model in the config.yaml.
    - Debug logging has also been added to make things easier to...
    	debug.
    - Loading the config and initializing the whisper model ahs been
    	moved into their own functions, and are now run with the if
    	__name__ == "__main__" thing.

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ import threading
 import queue
 import os
 import whisper
+import logging
+import yaml
 
-# TODO: Set up logging instead of print statements
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] %(message)s')
 filename_queue = queue.Queue()  # Create a queue to store the filenames
 transcription_queue = queue.Queue()
 file_written_event = threading.Event()
-print("Loading model. This could take a while...")
-whisper_model = whisper.load_model("tiny.en")
-print("Loaded!")
+
+def load_config():
+    with open("config.yaml") as f:
+        loaded_config = yaml.load(f, Loader=yaml.FullLoader)
+    return loaded_config
+
+def init_whisper(config_loaded):
+    logging.info("Loading model. This could take a while...")
+    model_name = config_loaded["whisper"]["model"]
+    init_whisper_model = whisper.load_model(model_name)
+    logging.info("Model loaded!")
+    return init_whisper_model
 
 def record_audio(seconds):
     sample_rate = 44100
@@ -21,7 +32,7 @@ def record_audio(seconds):
         directory = "temp_recordings"
         filename = f"{random.randint(0, 999999999)}.wav"  # There's a 1/999999999 chance of a collision. Whoops.
         filepath = os.path.join(directory, str(filename))
-        print("Recording...")
+        logging.debug("Recording to %s...", filepath)
         recording = sd.rec(
             int(seconds * sample_rate), samplerate=sample_rate, channels=2
         )
@@ -30,19 +41,18 @@ def record_audio(seconds):
 
         # Helps to avoid a race condition where the file is read/attempted to be transcribed before it is written
         file_written_event.set()
-        print("Done recording!")
+        logging.debug("File written to %s!", filepath)
 
         # Put the filename into the queue
         filename_queue.put(filename)
 
 
-def process_audio():
+def process_audio(loaded_whisper_model):
     while True:
         filename = filename_queue.get()
         file_written_event.wait()
-        # print(filename)
-        transcription = whisper_model.transcribe(f"temp_recordings/{filename}")
-        print(transcription["text"])
+        transcription = loaded_whisper_model.transcribe(f"temp_recordings/{filename}")
+        logging.debug("Transcription: " + transcription["text"])
         transcription_queue.put(transcription["text"])
         file_written_event.clear()
 
@@ -69,14 +79,20 @@ def check_badwords_string():
         transcribe = transcription_queue.get()
         badwords_exist, detected_badwords = return_badwords(transcribe)
         if badwords_exist:
-            print(f"Bad word(s) detected! {detected_badwords}")
+            logging.debug("Badwords detected! " + str(detected_badwords))
 
-# Create two threads, one for each task
-record_thread = threading.Thread(target=record_audio, args=(15,))
-process_thread = threading.Thread(target=process_audio)
-check_thread = threading.Thread(target=check_badwords_string)
 
-# Start the threads!!!
-record_thread.start()
-process_thread.start()
-check_thread.start()
+if __name__ == "__main__":
+    config = load_config()
+    whisper_model = init_whisper(config)
+    recorder_seconds = config["recorder"]["seconds"]
+
+    # Create two threads, one for each task
+    record_thread = threading.Thread(target=record_audio, args=(recorder_seconds,))
+    process_thread = threading.Thread(target=process_audio, args=(whisper_model,))
+    check_thread = threading.Thread(target=check_badwords_string)
+
+    # Start the threads!!!
+    record_thread.start()
+    process_thread.start()
+    check_thread.start()
